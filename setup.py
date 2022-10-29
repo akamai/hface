4ba9ac8df233093e403f79d589b3171aad02e348
Miloslav Pojman <mpojman@akamai.com>
2022-10-29 12:57:50 +0200

    Initial commit - setup.py to reserver the name at PyPI.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="hface",
+    version="0.0",
+    author="Miloslav Pojman",
+    author_email="mpojman@akamai.com",
+    description="Hackable HTTP/{1,2,3} {client,server,proxy}",
+    long_description="Coming soon..."
+)

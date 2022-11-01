1cbcd63dd6fb529ec6b59a8bda62f5c2e1e1b18d
Miloslav Pojman <mpojman@akamai.com>
2022-11-01 23:34:12 +0100

    Increment version after the release.

--- src/hface/__init__.py
+++ src/hface/__init__.py
@@ -16,4 +16,4 @@ from ._configuration import ClientTLSConfig, ServerTLSConfig
 from ._error_codes import HTTPErrorCodes
 from ._typing import AddressType, DatagramType, HeadersType, HeaderType
 
-__version__ = "0.1"
+__version__ = "0.2.dev"

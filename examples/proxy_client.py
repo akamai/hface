22eaf6318185bd6fccf9b7e7d1f4389e0661c162
Miloslav Pojman <mpojman@akamai.com>
2022-11-01 22:45:49 +0100

    Proxy client example reads a whole response.

--- examples/proxy_client.py
+++ examples/proxy_client.py
@@ -1,5 +1,7 @@
 import asyncio
 
+from anyio import EndOfStream
+
 from hface.client import ProxyClient
 
 
@@ -7,9 +9,18 @@ async def main():
     proxy_client = ProxyClient("https://localhost:6443")
     async with proxy_client.session() as session:
         stream = await session.connect_tcp_tls(("localhost", 5443))
-        await stream.send(b"GET / HTTP/1.1\r\nHost: localhost\r\n\r\n")
-        response = await stream.receive()
-    print(response.decode())
+        await stream.send(
+            b"GET / HTTP/1.1\r\n"
+            b"Host: localhost\r\n"
+            b"Connection: close\r\n"
+            b"\r\n"
+        )
+        while True:
+            try:
+                chunk = await stream.receive()
+            except EndOfStream:
+                break
+            print(chunk.decode(), end="")
 
 
 if __name__ == "__main__":
